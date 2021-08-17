2d59d3301f442aed2ab4f26c9060314b7473efc6
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-18 04:00:37 +0600

    learned array declaration and array length

---
+++ variable.js
@@ -0,0 +1,16 @@
+// Number type 
+var watchPrice = 120;
+watchPrice = 125;
+
+// String Type 
+var userName = 'Sogir Uddin'
+
+// Boolean Type 
+
+var isExpensive = true;
+
+// integer : 1 2 3 4 
+
+// float : 2.713, 238.23
+
+
